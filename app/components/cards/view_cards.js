ddfa19520bd93823342f8c4df19b785c4d1fa811
Giruta <irina.giruta@gmail.com>
2022-01-19 11:35:32 +0200

    add some bootstrap classes6 change html/css code

--- app/components/cards/view_cards.js
+++ app/components/cards/view_cards.js
@@ -1,5 +1,5 @@
 export default class ViewCards {
-  BODY = document.body.querySelector('.flex');
+  BODY = document.body.querySelector('.content');
 
   renderSneakers(sneakers) {
     this.BODY.insertAdjacentHTML('beforeend', sneakers.map(this.getSneakersHTML).join(''));
@@ -7,17 +7,19 @@ export default class ViewCards {
 
   getSneakersHTML({ model, image, description, price }) {
     return `
-        <div class="card"">
+      <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
+        <div class="card mb-4 h-100">
           <img src="${image}" class="card-img-top" alt="${model}">
           <div class="card-body">
             <h5 class="card-title">${model}</h5>
             <p class="card-text">${description}</p>
-            <div class="card-action">
+          </div>
+          <div class="card-footer bg-transparent">
             <h5 class="price">${price}</h5>
             <a href="#" class="btn btn-lg btn-dark">Add <i class="bi bi-bag-plus-fill"></i></a>
-            </div>
           </div>
         </div>
+      </div>  
     `;
   }
 }
