63606881f54f1ce96d09c55161bb5c5fb5acc490
Yevhen Dovhan <y.d.devbox@gmail.com>
2022-01-24 10:30:30 +0200

    Small css changes

--- app/components/cards/view_cards.js
+++ app/components/cards/view_cards.js
@@ -14,13 +14,17 @@ export default class ViewCards {
   }
 
   //  HTML template for product card
-  getSneakersHTML({ id, model, image, description, price, brand }) {
+  getSneakersHTML({ id, model, image, description, price, brand, gender, size }) {
     return `
         <div class="card" data-id="${id}">
           <img src="${image}" class="card-img-top" alt="${model}">
           <div class="card-body">
             <h5 class="card-title">${model}</h5>
-            <h5 class="card-subtitle mb-2 text-muted">${brand}</h5>
+            <div class="card-info">
+              <h5 class="card-subtitle mb-2 text-muted">${brand}</h5>
+              <h5 class="card-subtitle mb-2 text-muted">gender: ${gender}</h5>
+              <h5 class="card-subtitle mb-2 text-muted">size: ${size}</h5>
+            </div>
             <p class="card-text">${description}</p>
             </div>
           <div class="card-footer bg-transparent d-flex justify-content-between align-content-start align-items-center">
